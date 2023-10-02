88cbf66d9edcd7aee64eb80dcb7810be7ed456c9
luciangrx <luciangrr@gmail.com>
2023-10-02 15:45:25 +0300

    feat:Proiect nou cu Nextjs si Strapi

---
+++ frontend/pages/index.js
@@ -0,0 +1,35 @@
+import Head from 'next/head';
+import { useQuery } from 'urql';
+import { PRODUCT_QUERY } from '@/lib/query';
+import Product from '@/components/Product';
+import { Gallery } from '@/styles/Gallery';
+
+
+export default function Home() {
+
+  const [results] = useQuery({query:PRODUCT_QUERY});
+  const {data, fetching, error} = results;
+
+  if(fetching) return <p>Loading...</p>
+  if(error) return <p>{error.message}</p>
+  
+  const products = data.products.data;
+
+
+  return(
+    <div>
+      <Head>
+        <title>StepUpStyle</title>
+        <meta name='description' content='Apps'></meta>
+        <link rel='icon' href='/favicon.ico'></link>
+      </Head>
+
+      <main>
+        <Gallery>
+        {products.map((product) => (
+        <Product key={product.attributes.slug} product={product}/>
+        ))}
+        </Gallery>
+      </main>
+    </div>
+  )};
